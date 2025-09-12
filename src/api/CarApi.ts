bf88cea382a178b16022b6d15efd0e539eab1753
ghdtjrals702-svg <ghdtjrals702@gmail.com>
2025-09-12 14:11:15 +0900

    Initial commit

---
+++ src/api/CarApi.ts
@@ -0,0 +1,35 @@
+import axios, { type AxiosRequestConfig } from "axios";
+import type { Car } from "../type"
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+const getAxiosConfig = (): AxiosRequestConfig => {
+    const token = sessionStorage.getItem('jwt');
+    return {
+        headers: {
+            'Authorization': token
+        }
+    }
+}
+
+// 자동차 목록 조회
+export const getCars = async (): Promise<Car[]> => {
+    const response = await axios.get(`${BASE_URL}/cars`, getAxiosConfig());
+    return response.data;
+   
+} 
+
+export const deleteCar = async (id: number): Promise<number> => {
+   const response = await axios.delete(`${BASE_URL}/cars/${id}`, getAxiosConfig());
+   return response.data;
+}
+
+export const addCar = async (car: Car): Promise<Car> => {
+    const response = await axios.post(`${BASE_URL}/cars`,car, getAxiosConfig());
+    return response.data;
+}
+
+export const updateCar = async (car: Car): Promise<Car> => {
+    const response = await axios.put(`${BASE_URL}/cars`,car, getAxiosConfig());
+    return response.data;
+}
---
+++ src/pages/Login.tsx
@@ -0,0 +1,67 @@
+import { Button, Snackbar, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+import { GetAuthToken } from "../api/LoginApi";
+import type { User } from "../type";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store";
+
+
+
+export default function Login() {
+    const navigate = useNavigate();
+    const { login } = useAuthStore();
+    const [toastOpen, setToastOpen] = useState(false);
+    const [user, setUser] = useState<User>({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({...user, [e.target.name]: e.target.value})
+    };
+
+    const handleLogin = () => {
+        GetAuthToken(user)
+        .then((token) => {
+            if(token !== null) {
+                sessionStorage.setItem("jwt", token);
+                login();
+                navigate("/");
+            }
+            
+        })
+        .catch((err) => {
+            console.log(err);
+            setToastOpen(true);
+        });
+    };
+
+    return (
+        <>
+        <Stack spacing={2} mt={2} alignItems="center">
+            <TextField
+            label="ID"
+            name="username" 
+            onChange={handleChange}
+            />
+            <TextField
+            label="PW"
+            name="password" 
+            onChange={handleChange}
+            />
+            <Button
+                color="primary"
+                onClick={handleLogin}
+            >
+                로그인
+            </Button> 
+            <Snackbar 
+                open={toastOpen}
+                autoHideDuration={3000}
+                onClose={() => setToastOpen(false)}
+                message='로그인 실패'
+            />   
+        </Stack>
+        </>
+    )
+}
